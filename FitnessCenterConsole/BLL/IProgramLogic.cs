d4903166bdf25af7ddecd47ff4ad0a09782bc62c
AndrewSalygin <andrew.salygin@gmail.com>
2023-05-12 21:58:36 +0400

    did three layers

--- FitnessCenterConsole/BLL/IProgramLogic.cs
+++ FitnessCenterConsole/BLL/IProgramLogic.cs
@@ -1,22 +1,30 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using FitnessCenterConsole.Entities;
 
-namespace FitnessCenterConsole.DAL {
-    public interface IDatabase {
+namespace FitnessCenterConsole.BLL
+{
+    public interface IProgramLogic {
         public bool AddNewCoach(string surname, string name, string middleName, int experience,
             Education education, DateTime birthday, string phoneNumber);
-        public bool AddNewClient(string surname, string name, string middleName,
-            DateTime birthday, string phoneNumber);
+
+        public bool AddNewClient(string surname, string name, string middleName, DateTime birthday, 
+            string phoneNumber);
+
         public bool AddNewGym(int numberOfGym, TypeOfGym typeOfGym);
+
         public bool AddClientToTraining(string surnameClient, string phoneNumberClient,
             string surnameCoach, string phoneNumberCoach, DateTime dateTime);
+
         public bool DeleteClientFromTraining(string surnameClient, string phoneNumberClient,
             string surnameCoach, string phoneNumberCoach, DateTime dateTime);
-        public bool AddNewTraining(int gymKey, string surnameCoach, string phoneNumberCoach,
-            HashSet<string> clientKeys, DateTime dateTime);
-        public bool AddNewTraining(int gymKey, string surnameCoach,
-            string phoneNumberCoach, DateTime dateTime);
+
+        public bool AddNewTraining(int gymKey, string surnameCoach, string phoneNumberCoach, HashSet<string> clientKeys,
+            DateTime dateTime);
+
+        public bool AddNewTraining(int gymKey, string surnameCoach, string phoneNumberCoach, DateTime dateTime);
+        public bool AddNewTraining(int gymKey, DateTime dateTime);
         public bool DeleteTraining(int gymKey, DateTime dateTime);
         public string GetInfoCoach(string surname, string phoneNumber);
         public string GetInfoGym(int numberOfGym);
@@ -27,8 +35,6 @@ namespace FitnessCenterConsole.DAL {
         public string FindTrainingCoach(string surname, string phoneNumber, DateTime dateTime);
         public string FindTrainingClient(string surname, string phoneNumber, DateTime dateTime);
         public string FindTrainingGym(int numberOfGym, DateTime dateTime);
-        bool AddNewTraining(int gymKey, DateTime surnameCoach);       
         public void saveDatabase(string file);
-        public IDatabase setDatabase(string file);
-    }
+        }
 }
